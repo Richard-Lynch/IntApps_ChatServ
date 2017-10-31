ad609fa925a734ebb287cfac088205a802392d42
Richard Lynch <rich753@hotmail.com>
2017-10-31 11:34:52 +0000

    added ip options

--- infoClient.py
+++ infoClient.py
@@ -2,13 +2,15 @@
 
 from client import client
 import sys
-
+ip = "localhost"
 message = "HELO text\n"
 if len(sys.argv) > 1:
     print ("Port: ", sys.argv[1])
     port = int(sys.argv[1])
+    if len(sys.argv) > 2:
+        ip = str(sys.argv[2])
 else:
     print ("port number needed")
     exit()
 
-client ('localhost', port, message)
+client (ip, port, message)
--- testClient.py
+++ testClient.py
@@ -2,15 +2,17 @@
 
 from client import client
 import sys
-
+ip = "localhost"
 message = "hello world"
 if len(sys.argv) > 1:
     print ("Port: ", sys.argv[1])
     port = int(sys.argv[1])
     if len(sys.argv) > 2:
         message = str(sys.argv[2]).replace("\\n", "\n")
+    if len(sys.argv) > 3:
+        ip = str(sys.argv[3])
 else:
     print ("port number needed")
     exit()
 
-client ('localhost', port, message)
+client (ip, port, message)
